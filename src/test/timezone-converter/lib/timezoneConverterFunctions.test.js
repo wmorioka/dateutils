493bf82a67d2fc8c73a72d5e6cb0cfed10e5d620
wmorioka <jp.wmorioka@gmail.com>
2024-05-26 21:24:03 +0900

    Add saveConvertHistory and getConvertHistory test

--- src/test/timezone-converter/lib/timezoneConverterFunctions.test.js
+++ src/test/timezone-converter/lib/timezoneConverterFunctions.test.js
@@ -1,6 +1,6 @@
-import { validateDatetimeFormat } from '../../../timezone-converter/lib/timezonConverterFunctions'
+import { validateDatetimeFormat, saveConvertHistory, getConvertHistory } from '../../../timezone-converter/lib/timezonConverterFunctions'
 import { describe, expect, it, vi } from 'vitest'
-
+import dayjs from 'dayjs'
 describe('validateDatetimeFormat', () => {
     describe('date is valid', () => {
         it('returns true', () => {
@@ -28,6 +28,44 @@ describe('validateDatetimeFormat', () => {
             validDatePatterns.forEach(date => {
                 expect(validateDatetimeFormat(date)).toBeFalsy()
             });
+        })
+    })
+})
+const cconvertHistoryStorageKey = '/v1/convert-history'
+describe('saveConvertHistory', () => {
+    beforeEach(async () => {
+        localStorage.clear()
+    })
+    it('can save data in localStorage', () => {
+        const history = {}
+        history.date = dayjs().format('YYYY/MM/DD HH:mm')
+        history.timezoneFrom = { id: "UTC+9:00_JST", label: "(UTC+9:00) Japan Standard Time (JST) - Asia" }
+        history.timezoneTo = { id: "UTC+5:30_IST", label: "(UTC+5:30) India Standard Time (IST) - Asia" }
+        history.isAbbreviationChecked = false
+        history.isUTCOffsetChecked = false
+        expect(saveConvertHistory(history)).toBeTruthy()
+        expect(JSON.parse(localStorage.getItem(cconvertHistoryStorageKey))).toMatchObject(history)
+    })
+})
+describe('getConvertHistory', () => {
+    beforeEach(async () => {
+        localStorage.clear()
+    })
+    describe('data exists in localStorage', () => {
+        it('returns saved data', () => {
+            const history = {}
+            history.date = dayjs().format('YYYY/MM/DD HH:mm')
+            history.timezoneFrom = { id: "UTC+9:00_JST", label: "(UTC+9:00) Japan Standard Time (JST) - Asia" }
+            history.timezoneTo = { id: "UTC+5:30_IST", label: "(UTC+5:30) India Standard Time (IST) - Asia" }
+            history.isAbbreviationChecked = false
+            history.isUTCOffsetChecked = false
+            localStorage.setItem(cconvertHistoryStorageKey, JSON.stringify(history))
+            expect(getConvertHistory()).toMatchObject(history)
+        })
+    })
+    describe('data does not exist in localStorage', () => {
+        it('returns null', () => {
+            expect(getConvertHistory()).toBeNull()
         })
     })
 })
